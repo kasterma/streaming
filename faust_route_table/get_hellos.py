db7f7545572ea1d8a93084acc32a4461acf52af6
Bart Kastermans <kasterma@kasterma.net>
2020-10-20 14:46:52 +0200

    WIP: cleaned up example

--- faust_route_table/get_hellos.py
+++ faust_route_table/get_hellos.py
@@ -1,30 +1,18 @@
 import faust
-import asyncio
-import random
-
-from faust import Schema
 
 app = faust.App("get-hello")
 
 hello_topic = app.topic("hellos-here")
-hello_counts = app.Table('hello_counts', default=int, schema=Schema(key_type=bytes, key_serializer="raw"))  # key_serializer="raw",
-
-print(f"hi {hello_counts.schema}")
+hello_counts = app.Table('hello_counts', default=int)
 
 @app.agent(hello_topic)
 async def get_hello(t):
-    async for e in t:   # .group_by(lambda ee: str(ee), name="value_grouped"):
+    async for e in t:
         print(e)
         print(app.conf)
         hello_counts[e.encode()] += 1
         for k,v in hello_counts.items():
             print(f"{k}: {v}")
-        #print(hello_counts.as_ansitable(tty=False))
-        # for i in range(11):  # print the whole table
-        #     hh2 = f"hellohello-{i}"
-        #     print(f"count {i} is {hello_counts[hh2]}")
-        #await asyncio.sleep(1)  # this was here to see about comits of messages, didn't finish the experiment to see reprocessing happening
-        #print(f"commit {e}")
 
 @app.page('/count/')
 @app.table_route(table=hello_counts, query_param='i')
@@ -32,10 +20,10 @@ async def get_count(web, request):
     i = request.query['i']
     print(f"request {i}")
     try:
-        print(f"result {hello_counts[i.encode()]}")
+        print(f"result {hello_counts[i]}")
     except:
-        print(f"erro getting {i.encode()}")
-    return web.json({i: hello_counts[i.encode()]})
+        print(f"erro getting {i}")
+    return web.json({i: hello_counts[i]})
 
 if __name__ == "__main__":
     app.main()
