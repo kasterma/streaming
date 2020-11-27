250a83a5017321038ffa36c65b01abc9de08403f
Bart Kastermans <kasterma@kasterma.net>
2020-11-27 16:25:14 +0100

    WIP: ready for next steps wouter

--- trace_faust/send_events.py
+++ trace_faust/send_events.py
@@ -11,13 +11,17 @@ app = faust.App("send-events")
 event_topic = app.topic("raw-events")
 event_counts_send = defaultdict(int)
 
+id = 0
+
 @app.timer(interval=1)
 async def send_events():
+    global id
     with tracer.start_span('send-event') as span:
         val = random.choice(["a", "b", "c", "d", "e", "f"])
         span.set_tag("choice", val)
         event_counts_send[val] += 1
-        await event_topic.send(value=val)
+        await event_topic.send(key=val, value=val, headers={'id': str(id).encode()})
+        id += 1
         span.log_kv({"send": True, "val": val})
         print(json.dumps(event_counts_send, sort_keys=True))
 
